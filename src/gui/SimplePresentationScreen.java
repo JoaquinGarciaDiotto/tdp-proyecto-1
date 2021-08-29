3dfcfdc8000c76201c25535a89e1b0ea315deead
Skorrom <73122602+Skorrom@users.noreply.github.com>
2021-08-29 12:04:40 -0300

    Finalizo Proyecto

--- src/gui/SimplePresentationScreen.java
+++ src/gui/SimplePresentationScreen.java
@@ -9,6 +9,17 @@ import javax.swing.border.EmptyBorder;
 
 import entities.Student;
 import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import javax.imageio.ImageIO;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 @SuppressWarnings("serial")
 public class SimplePresentationScreen extends JFrame {
@@ -17,8 +28,20 @@ public class SimplePresentationScreen extends JFrame {
 	private JPanel tabInformation;
 	private JTabbedPane tabbedPane;
 	private Student studentData;
+	private JLabel labelLU;
+	private JLabel labelApellido;
+	private JLabel labelNombre;
+	private JLabel labelEmail;
+	private JLabel labelGithub;
+	private JLabel labelFecha;
+	private JTextField txtGithub;
+	private JTextField txtEmail;
+	private JTextField txtLU;
+	private JTextField txtApellido;
+	private JTextField txtNombre;
 
 	public SimplePresentationScreen(Student studentData) {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(SimplePresentationScreen.class.getResource("/images/tdp.png")));
 		this.studentData = studentData;
 		
 		contentPane = new JPanel();
@@ -27,7 +50,7 @@ public class SimplePresentationScreen extends JFrame {
 		
 		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentación");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(new Dimension(615, 250));
+		setSize(new Dimension(747, 250));
 		setResizable(false);
 		setContentPane(contentPane);
 		
@@ -40,6 +63,92 @@ public class SimplePresentationScreen extends JFrame {
 		tabInformation = new JPanel();
 		tabInformation.setPreferredSize(new Dimension(425, 275));
 		tabbedPane.addTab("Información del alumno", null, tabInformation, "Muestra la información declarada por el alumno");
+		
+		labelLU = new JLabel("LU");
+		
+		labelApellido = new JLabel("Apellido");
+		
+		labelNombre = new JLabel("Nombre");
+		
+		labelEmail = new JLabel("E-mail");
+		
+		labelGithub = new JLabel("Github URL");
+		
+		txtGithub = new JTextField();
+		txtGithub.setText(studentData.getGithubURL());
+		txtGithub.setColumns(10);
+		
+		txtEmail = new JTextField();
+		txtEmail.setText(studentData.getMail());
+		txtEmail.setColumns(10);
+		
+		txtLU = new JTextField();
+		txtLU.setText(""+studentData.getId());
+		txtLU.setColumns(10);
+		
+		txtApellido = new JTextField();
+		txtApellido.setText(studentData.getLastName());
+		txtApellido.setColumns(10);
+		
+		txtNombre = new JTextField();
+		txtNombre.setText(studentData.getFirstName());
+		txtNombre.setColumns(10);
+		
+		GroupLayout gl_tabInformation = new GroupLayout(tabInformation);
+		gl_tabInformation.setHorizontalGroup(
+			gl_tabInformation.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_tabInformation.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.LEADING)
+						.addComponent(labelGithub)
+						.addComponent(labelEmail)
+						.addComponent(labelNombre)
+						.addComponent(labelLU)
+						.addComponent(labelApellido))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.LEADING)
+						.addComponent(txtLU, GroupLayout.DEFAULT_SIZE, 342, Short.MAX_VALUE)
+						.addComponent(txtApellido, GroupLayout.DEFAULT_SIZE, 342, Short.MAX_VALUE)
+						.addComponent(txtNombre, GroupLayout.DEFAULT_SIZE, 342, Short.MAX_VALUE)
+						.addComponent(txtEmail, GroupLayout.DEFAULT_SIZE, 342, Short.MAX_VALUE)
+						.addComponent(txtGithub, GroupLayout.DEFAULT_SIZE, 342, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		gl_tabInformation.setVerticalGroup(
+			gl_tabInformation.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_tabInformation.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.BASELINE)
+						.addComponent(labelLU)
+						.addComponent(txtLU, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.BASELINE)
+						.addComponent(labelApellido)
+						.addComponent(txtApellido, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.BASELINE)
+						.addComponent(labelNombre)
+						.addComponent(txtNombre, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.BASELINE)
+						.addComponent(labelEmail)
+						.addComponent(txtEmail, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.BASELINE)
+						.addComponent(labelGithub)
+						.addComponent(txtGithub, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(34, Short.MAX_VALUE))
+		);
+		tabInformation.setLayout(gl_tabInformation);
 		contentPane.add(tabbedPane, BorderLayout.WEST);
+		
+		labelFecha = new JLabel("Esta ventana fue genereada el "+java.time.LocalDate.now()+" a las: "+java.time.LocalTime.now());
+		contentPane.add(labelFecha, BorderLayout.SOUTH);
+		
+		try {
+			JLabel labelFoto = new JLabel("");
+			labelFoto.setIcon(new ImageIcon(ImageIO.read(getClass().getResourceAsStream(studentData.getPathPhoto()))));
+			contentPane.add(labelFoto, BorderLayout.EAST);
+		} catch (IOException e) {e.printStackTrace();}
 	}
 }
--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,12 +1,17 @@
 package launcher;
 
+import entities.Student;
+import gui.SimplePresentationScreen;
+
 public class Launcher {
 	public static void main(String [] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+		Student JoaquinGarciaDiotto = new Student(124996,"Garcia Diotto","Joaquin","joaquinmichu@hotmail.com","https://github.com/Skorrom","/images/foto.png");
+		SimplePresentationScreen GUI = new SimplePresentationScreen(JoaquinGarciaDiotto);
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-            	
+            	GUI.setVisible(true);
             }
         });
     }
